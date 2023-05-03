75b7e4530b7617efb9935b8dd751143798315c07
alslemos <alexandre-ls@hotmail.com>
2023-05-02 23:52:29 -0300

    atividade1

---
+++ Atividade1.java
@@ -0,0 +1,229 @@
+package atividadeUm;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTabbedPane;
+import java.awt.Color;
+import java.awt.BorderLayout;
+import javax.swing.ImageIcon;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextPane;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JCheckBox;
+import javax.swing.JEditorPane;
+import javax.swing.JToggleButton;
+import javax.swing.JSeparator;
+
+public class Prontuario extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Prontuario frame = new Prontuario();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Prontuario() {
+		setResizable(false);
+		setTitle("Prontuário Médico");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 600, 500);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.BOTTOM);
+		contentPane.add(tabbedPane);
+		
+		JPanel panel1 = new JPanel();
+		panel1.setBackground(new Color(219, 255, 140));
+		tabbedPane.addTab("Dados Pessoais", new ImageIcon(Prontuario.class.getResource("/icons/1.png")), panel1, null);
+		panel1.setLayout(new MigLayout("", "[][grow][grow][grow]", "[][][][][][][][][grow][]"));
+		
+		JLabel lblNewLabel_6 = new JLabel("Dados pessoais");
+		lblNewLabel_6.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
+		panel1.add(lblNewLabel_6, "cell 1 0");
+		
+		JLabel lblNewLabel = new JLabel("Nome");
+		panel1.add(lblNewLabel, "cell 0 1,alignx trailing");
+		
+		textField = new JTextField();
+		panel1.add(textField, "cell 1 1 3 1,growx");
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("CPF");
+		panel1.add(lblNewLabel_1, "cell 0 2,alignx trailing");
+		
+		textField_1 = new JTextField();
+		panel1.add(textField_1, "cell 1 2 3 1,growx");
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Cartão SUS");
+		panel1.add(lblNewLabel_2, "cell 0 3,alignx trailing");
+		
+		textField_2 = new JTextField();
+		panel1.add(textField_2, "cell 1 3 3 1,growx");
+		textField_2.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Plano privado");
+		panel1.add(lblNewLabel_3, "cell 0 4,alignx trailing");
+		
+		textField_3 = new JTextField();
+		panel1.add(textField_3, "cell 1 4 3 1,growx");
+		textField_3.setColumns(10);
+		
+		JLabel lblNewLabel_4 = new JLabel("Gênero");
+		panel1.add(lblNewLabel_4, "cell 0 6,alignx right");
+		
+		JRadioButton rdbtnNewRadioButton = new JRadioButton("Masculino");
+		panel1.add(rdbtnNewRadioButton, "flowx,cell 1 6 3 1,alignx left");
+		
+		JLabel lblNewLabel_5 = new JLabel("Condições conhecidas");
+		panel1.add(lblNewLabel_5, "cell 0 8");
+		
+		JTextPane textPane = new JTextPane();
+		panel1.add(textPane, "cell 1 8 3 1,grow");
+		
+		JButton btnNewButton = new JButton("Limpar");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setIcon(new ImageIcon(Prontuario.class.getResource("/icons/clean.png")));
+		panel1.add(btnNewButton, "flowx,cell 1 9");
+		
+		JButton btnNewButton_1 = new JButton("Salvar");
+		btnNewButton_1.setIcon(new ImageIcon(Prontuario.class.getResource("/icons/ok.png")));
+		panel1.add(btnNewButton_1, "cell 1 9");
+		
+		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Feminino");
+		panel1.add(rdbtnNewRadioButton_1, "cell 1 6 2 1");
+		
+		JRadioButton rdbtnNewRadioButton_2 = new JRadioButton("Outro");
+		panel1.add(rdbtnNewRadioButton_2, "cell 2 6");
+		
+		JPanel panel2 = new JPanel();
+		panel2.setBackground(new Color(200, 200, 200));
+		tabbedPane.addTab("Anamnese", new ImageIcon(Prontuario.class.getResource("/icons/2.png")), panel2, null);
+		panel2.setLayout(new MigLayout("", "[grow]", "[][][][][][][][][][][][grow]"));
+		
+		JLabel lblNewLabel_7 = new JLabel("Anamnese");
+		lblNewLabel_7.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
+		panel2.add(lblNewLabel_7, "cell 0 0");
+		
+		JLabel lblNewLabel_8 = new JLabel("Anamnese inicial");
+		panel2.add(lblNewLabel_8, "cell 0 1");
+		
+		textField_4 = new JTextField();
+		panel2.add(textField_4, "cell 0 2 1 2,growx");
+		textField_4.setColumns(10);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("Auscuta realizada");
+		panel2.add(chckbxNewCheckBox, "cell 0 4");
+		
+		JCheckBox chckbxNewCheckBox_1 = new JCheckBox("Aferição pressao sanguinea");
+		panel2.add(chckbxNewCheckBox_1, "cell 0 5");
+		
+		JCheckBox chckbxNewCheckBox_2 = new JCheckBox("Nível oxigenação");
+		panel2.add(chckbxNewCheckBox_2, "cell 0 6");
+		
+		JCheckBox chckbxNewCheckBox_3 = new JCheckBox("Temperatura corportal");
+		panel2.add(chckbxNewCheckBox_3, "cell 0 7");
+		
+		JCheckBox chckbxNewCheckBox_4 = new JCheckBox("Sintomas gripais");
+		panel2.add(chckbxNewCheckBox_4, "cell 0 8");
+		
+		JCheckBox chckbxNewCheckBox_5 = new JCheckBox("Sinais de fratura");
+		panel2.add(chckbxNewCheckBox_5, "cell 0 9");
+		
+		JLabel lblNewLabel_9 = new JLabel("Observações completas");
+		panel2.add(lblNewLabel_9, "cell 0 10");
+		
+		JEditorPane editorPane = new JEditorPane();
+		panel2.add(editorPane, "cell 0 11,grow");
+		
+		JPanel panel3 = new JPanel();
+		panel3.setBackground(new Color(100, 100, 100));
+		tabbedPane.addTab("Exames", new ImageIcon(Prontuario.class.getResource("/icons/3.png")), panel3, null);
+		panel3.setLayout(new MigLayout("", "[grow]", "[][][][][][][][][grow]"));
+		
+		JLabel lblNewLabel_10 = new JLabel("Exames solicitados");
+		lblNewLabel_10.setForeground(new Color(255, 255, 255));
+		lblNewLabel_10.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
+		panel3.add(lblNewLabel_10, "cell 0 0");
+		
+		JSeparator separator = new JSeparator();
+		panel3.add(separator, "cell 0 1");
+		
+		JCheckBox chckbxNewCheckBox_6 = new JCheckBox("Exame 1");
+		chckbxNewCheckBox_6.setForeground(new Color(255, 255, 255));
+		panel3.add(chckbxNewCheckBox_6, "cell 0 2");
+		
+		JCheckBox chckbxNewCheckBox_7 = new JCheckBox("Exame 2");
+		chckbxNewCheckBox_7.setForeground(new Color(255, 255, 255));
+		panel3.add(chckbxNewCheckBox_7, "cell 0 3");
+		
+		JCheckBox chckbxNewCheckBox_8 = new JCheckBox("Exame 3");
+		chckbxNewCheckBox_8.setForeground(new Color(255, 255, 255));
+		panel3.add(chckbxNewCheckBox_8, "cell 0 4");
+		
+		JCheckBox chckbxNewCheckBox_9 = new JCheckBox("Exame 4");
+		chckbxNewCheckBox_9.setForeground(new Color(255, 255, 255));
+		panel3.add(chckbxNewCheckBox_9, "cell 0 5");
+		
+		JLabel lblNewLabel_11 = new JLabel("Resultados significativos anteriores");
+		lblNewLabel_11.setForeground(new Color(255, 255, 255));
+		panel3.add(lblNewLabel_11, "cell 0 7");
+		
+		JEditorPane editorPane_1 = new JEditorPane();
+		panel3.add(editorPane_1, "cell 0 8,grow");
+		
+		JPanel panel4 = new JPanel();
+		panel4.setBackground(new Color(50, 50, 150));
+		tabbedPane.addTab("Tratamentos", new ImageIcon(Prontuario.class.getResource("/icons/4.png")), panel4, null);
+		panel4.setLayout(new MigLayout("", "[61px,grow]", "[16px][][grow][][][][][][][]"));
+		
+		JLabel lblNewLabel_12 = new JLabel("Tratamentos indicados");
+		lblNewLabel_12.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
+		lblNewLabel_12.setForeground(new Color(255, 255, 255));
+		panel4.add(lblNewLabel_12, "cell 0 0");
+		
+		JLabel lblNewLabel_13 = new JLabel("Siga-os por gentileza");
+		lblNewLabel_13.setForeground(new Color(198, 198, 198));
+		panel4.add(lblNewLabel_13, "cell 0 1");
+		
+		JTextPane textPane_1 = new JTextPane();
+		panel4.add(textPane_1, "cell 0 2 1 8,grow");
+	}
+
+}
